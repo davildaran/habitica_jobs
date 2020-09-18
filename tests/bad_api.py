96f0702038cc6c3ae97c0f7e08f2b37818c43da9
David Tahvildaran <davildaran@gmail.com>
2020-09-17 23:22:02 -0400

    add initial tests

---
+++ tests/bad_api.py
@@ -0,0 +1,29 @@
+import requests
+import os
+import json
+
+# from common import constants
+# from secret import user_id, api_key
+
+HABITICA_API_URL = "https://habitica.com/api/v3_bad_url"
+user_id = "f"
+api_key = "0"
+
+def set_auth(_user_id, _api_key):
+	return {'Content-Type': 'application/json',
+			'x-api-user': _user_id,
+			'x-api-key': _api_key}
+
+
+def test_habitica_api_status():
+	r = requests.get(url=os.path.join(HABITICA_API_URL, 'status'),
+					headers=set_auth(user_id, api_key))
+	assert r.status_code == 200
+	data = json.loads(r.text)
+	assert data['data']['status'] == 'up'
+	assert data['success']
+	print('API Up status test passed')
+
+
+if __name__ == "__main__":
+	test_habitica_api_status()
