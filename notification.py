3e238fccec274197e499b05d2182feb16169a38d
David Tahvildaran <davildaran@gmail.com>
2020-09-17 23:46:44 -0400

    add notification module

---
+++ notification.py
@@ -0,0 +1,18 @@
+import yagmail
+
+
+receiver = "davildaran@gmail.com"
+body = "do you have issues committing to things?"
+
+
+def send_email():
+    yag = yagmail.SMTP("davildaran@gmail.com")
+    yag.send(
+        to=receiver,
+        subject="habitica jobs commit",
+        contents=body,
+    )
+
+
+if __name__ == "__main__":
+    send_email()
